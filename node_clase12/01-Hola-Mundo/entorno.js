8da45414b610e5c6e5a76b0c60d8ca409cda6476
Maximiliano <alsinamaxi@gmail.com>
2023-11-08 11:37:44 -0300

    Clase 12 node Modulos

---
+++ node_clase12/01-Hola-Mundo/entorno.js
@@ -0,0 +1,8 @@
+
+let nombre = process.env.NOMBRE || "sin nombre";
+let web = process.env.WEB || "sin web";
+
+console.log('Hola '+nombre);
+console.log('Mi web es: '+web);
+
+console.log('Ultima ejecucion');
